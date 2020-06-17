12ea3ffb5f517eb1b8f32e015161cabcfc656baf
luanhd <luanhd@solashi.com>
2020-06-17 13:34:08 +0700

    edit link

--- logout.php
+++ logout.php
@@ -2,14 +2,29 @@
 require_once('include/function.php');
 $token= get_param("token");
 if ($_SESSION["token"]==$token && isset($_SESSION['logged-in'])){
-	unset($_SESSION['logged-in']);//Hủy bỏ phiên làm việc
+
+	unset($_SESSION['logged-in']);
+	//Hủy bỏ phiên làm việc
+	// unset all session
+	session_destroy();
+
+
 	header('Location: login.php');
-}else if ($_SESSION['id_role'] == "Admin") {
+	exit;
+}
+if ($_SESSION['id_role'] == "Admin") {
 	header('Location: admin.php');
-}else if ($_SESSION['id_role'] == "Manager") {
+
+	exit;
+}
+
+if ($_SESSION['id_role'] == "Manager") {
 	header('Location: manager.php');
-}else if ($_SESSION['id_role'] == "User") {
+	exit;
+}
+if ($_SESSION['id_role'] == "User") {
 	header('Location: user.php');
+	exit;
 }
 // unset($_SESSION['logged-in']);//Hủy bỏ phiên làm việc
 // header('Location: login.php');
